dfc4ad8ac0138becbcf5f03551c84952287ce172
Ben <benymng@gmail.com>
2020-09-05 19:02:27 -0600

    made the index page work to display current information of product as associated with user

--- app.py
+++ app.py
@@ -10,20 +10,49 @@ login_manage = LoginManager()
 
 
 #username just for testing
-username = "bennyhin"
+username = "testing"
 
-# def main():
-    # print(scrape.find_web_price(page_soup))
-    # print(scrape.find_sale_price(page_soup))
-    # print(scrape.find_processor_info(page_soup))
-    # cur.execute("INSERT INTO users (username, password) VALUES ('asjdfkl', 'blah')")
-    # con.commit()
-    # con.close()
+# print(scrape.find_sale_price(page_soup))
+# print(scrape.find_processor_info(page_soup))
+# cur.execute("INSERT INTO users (username, password) VALUES ('asjdfkl', 'blah')")
+# con.commit()
+# con.close()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    con = lite.connect("information.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM information WHERE username='%s'" % username)
+    information = cur.fetchall()
+
+    #create list of everything
+    url_list = []
+    product_list = []
+    for info in information:
+        product_list.append(info[2])
+        url_list.append(info[3])
+
+    #get all information on the products associated with user in database
+    web_price_list = []
+    sale_price_list = []
+    processor_list = []
+    for i in range(len(information)):
+        web_price_list.append(scrape.find_web_price(scrape.find_html(url_list[i])))
+        sale_price_list.append(scrape.find_sale_price(scrape.find_html(url_list[i])))
+        processor_list.append(scrape.find_processor_info(scrape.find_html(url_list[i])))
+
+    #put all lists into a dictionary
+    product_info = {}
+    product_info["product"] = product_list
+    product_info["old_price"] = web_price_list
+    product_info["sale_price"] = sale_price_list
+    product_info["processor"] = processor_list
+
+    con.close()
+    
+    #return the dictionary of all of the product info and the number of products associated with the user
+    return render_template("index.html", product_info=product_info, num_product=len(information))
 
 
 @app.route("/history")
--- scrape.py
+++ scrape.py
@@ -11,38 +11,27 @@ def find_html(my_url):
     return page_soup
 
 
-# create a list of the prices of the product before discount
+# get the price of the base model before discount
 def find_web_price(page_soup):
     containers = page_soup.findAll("div", {"class": "tabbedBrowse-productListing"})
     container = containers[0]
-    web_price_list = []
-    for container in containers:
-        price_container = container.findAll("dd", {"class": "saleprice pricingSummary-priceList-value ls-has-discount"})
-        web_price = price_container[0].text
-        web_price_list.append(web_price)
-    return web_price_list
+    price_container = container.findAll("dd", {"class": "saleprice pricingSummary-priceList-value ls-has-discount"})
+    web_price = price_container[0].text
+    return web_price
 
 
 # create a list of the prices of the products after discount
 def find_sale_price(page_soup):
     containers = page_soup.findAll("div", {"class": "tabbedBrowse-productListing"})
     container = containers[0]
-    sale_price_list = []
-    for container in containers:
-        price_container = container.findAll("dd", {"class": "saleprice pricingSummary-details-final-price"})
-        sale_price = price_container[0].text
-        sale_price_list.append(sale_price)
-    return sale_price_list
+    price_container = container.findAll("dd", {"class": "saleprice pricingSummary-details-final-price"})
+    sale_price = price_container[0].text
+    return sale_price
 
 
 #create a list of the processor information
 def find_processor_info(page_soup):
     containers = page_soup.findAll("div", {"class": "tabbedBrowse-productListing-featureList featureList-bulleted featureList-linedRows"})
     container = containers[0]
-    print (len(containers))
-    print(container.dl.dd.text)
-    processor_list = []
-    for container in containers:
-        processor_name = container.dl.dd.text
-        processor_list.append(processor_name)
-    return processor_list
+    processor_name = container.dl.dd.text
+    return processor_name
